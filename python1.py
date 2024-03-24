09b8a6ea792b6f3a6892ca176d7adf75d7fbe612
Muhammad Aqsel <112957386+eevernexx@users.noreply.github.com>
2024-03-25 00:40:10 +0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,59 @@
+print("==============PYTHON BASIC 1==============")
+print("Hello World")
+print("\n")
+
+print("==============LATIHAN HOW TO TALK TO A COMPUTER==============")
+print("Katakan sesuatu...")
+sesuatu = input()
+print("mmmmm", sesuatu, " Oke")
+print("  \n  ")
+
+print("==============LATIHAN PRINT() FUNCTION==============")
+print("Hello, Python!")
+print("Abu")
+print('Abu')
+print('\n')
+
+print("==============LATIHAN FROMATTING THE OUTPUT==============")
+#clue
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
+print("  \n  ")
+
+print("==============LATIHAN VARIABLE A SIMPLE CONVERTER==============")
+kilometer = 12.25
+mil = 7.38
+mil_ke_kilometer = mil * 1.60934
+kilometer_ke_mil = kilometer / 1.60934
+print(mil, "mil adalah", round(mil_ke_kilometer, 2), "kilometer")
+print(kilometer, "kilometer adalah", round(kilometer_ke_mil, 2), "mil")
+print("  \n  ")
+
+
+print("==============LATIHAN REPLICATION==============")
+print("+" + 10*"-" + "+")
+print(("|" + 10*" " + "|\n") * 5, end="")
+print("+" + 10*"-" + "+")
+print("  \n  ")
+
+print("==============LATIHAN OPERATORS DAN EXPRESSIONS==============")
+print("\"I'm\"\n \"\"learning\"\"\n \"\"\"Python\"\"\"\n")
+
+x = 1
+y = 0.6
+print("x =", x)
+print("y =", round(y, 16))
+x = 10
+y = 0.09901951266867294
+print("\nx =", x)
+print("y =", round(y, 16))
+print("  \n  ")
+
+print("==============LATIHAN FORMATTING THE OUTPUT==============")
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****\n"*2)
